f57351db0a4fdf988d8b65e1c0745f166a7a9715
Esther <esthertarazona81@gmail.com>
2019-03-07 19:43:07 +0100

    fib

---
+++ PruebaFibonacci/src/code/FibonacciMain.java
@@ -0,0 +1,12 @@
+package code;
+
+public class FibonacciMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		FibonacciRecursive fbr= new FibonacciRecursive();
+		System.out.println(fbr.fibonacci(9));
+	}
+
+}
